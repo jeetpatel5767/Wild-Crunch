752fc0809cf411ebfd9b9f60a6cffefcba21830f
jeetpatel5767 <jjdb5767@gmail.com>
2025-09-23 09:00:49 +0530

    images2

--- src/components/Main Page/CrunchEraSection.tsx
+++ src/components/Main Page/CrunchEraSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Import your local images from assets
 import ProteinPuff from '@/assets/proteinpuff.png';
-import PopCorn from '@/assets/PopCorn.png';
+import PopCorn from '@/assets/popcorn.png';
 import CrunchyMakhana from '@/assets/crunchymakhana.png';
 import Makhana from '@/assets/Makhana.png';
 import PremiumMakhana from '@/assets/premiummakhana.png';
